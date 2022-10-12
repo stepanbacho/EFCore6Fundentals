9c361b0d0f6edcfaa74e43056b483e9e42781183
stepanbacho <78855270+stepanbacho@users.noreply.github.com>
2022-10-12 22:11:07 +0300

    first commit

---
+++ EFCore6Fundamentals/PublisherDomain/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace PublisherDomain
+{
+    public class Author
+    {
+        public Author()
+        {
+            Books = new List<Book>();
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public List<Book> Books { get; set; }
+    }
+}
