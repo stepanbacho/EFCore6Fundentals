50d2dd4ae596d2807301f231f5cffcf3e10bb589
stepanbacho <78855270+stepanbacho@users.noreply.github.com>
2022-10-12 22:12:10 +0300

    updated

--- EFCore6Fundamentals/PublisherDomain/Book.cs
+++ EFCore6Fundamentals/PublisherDomain/Book.cs
@@ -3,7 +3,7 @@
     public class Book
     {
         public int BookId { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = null!;
         public DateTime PublishDate { get; set; }
         public decimal BasePrice { get; set; }
         public Author Author { get; set; }
