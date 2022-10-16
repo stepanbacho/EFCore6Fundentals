f4e2bdd973022e5a49752dc2252da3aefd8366aa
stepanbacho <78855270+stepanbacho@users.noreply.github.com>
2022-10-17 02:04:27 +0300

    5 ch and logging done

--- EFCore6Fundamentals/PublisherConsole/Program.cs
+++ EFCore6Fundamentals/PublisherConsole/Program.cs
@@ -2,63 +2,80 @@
 using PublisherData;
 using PublisherDomain;
 
-using (PubContext context = new PubContext())
-{
-    context.Database.EnsureCreated();
-}
+//using (PubContext context = new PubContext())
+//{
+//    context.Database.EnsureCreated();
+//}
 //GetAuthors();
 //AddAuthor();
-//GetAuthors();
-AddAuthorWithBook();
-GetAuthorsWithBooks();
+
+//AddAuthorWithBook();
+//GetAuthorsWithBooks();
+PubContext _context = new PubContext();
+GetAuthors();
 
 
-void AddAuthor()
-{
-    var author = new Author { FirstName = "Josie", LastName = "Newf" };
-    using var context = new PubContext();
-    context.Authors.Add(author);
-    context.SaveChanges();
-}
-void AddAuthorWithBook()
-{
-    var auth = new Author { FirstName = "Julie", LastName = "Lerman" };
-    auth.Books.Add(new Book
-    {
-        Title = "Programming Entity Framework",
-        PublishDate = new DateTime(2009, 1, 1)
-    });
-    auth.Books.Add(new Book
-    {
-        Title = "Programming Entity Framework 2nd Ed",
-        PublishDate = new DateTime(2010, 8, 1)
-    });
-    using var context = new PubContext();
-    context.Authors.Add(auth);
-    context.SaveChanges();
-}
 
-void GetAuthorsWithBooks()
-{
-    using var context = new PubContext();
-    var auths = context.Authors.Include(a=> a.Books).ToList();
-    foreach (var auth in auths)
-    {
-        Console.WriteLine(auth.FirstName + " " + auth.LastName);
-        foreach (var book in auth.Books)
-        {
-            Console.WriteLine("*"+book.Title);
-        }
-    }
-}
+
+
+
+
+
+
+//QueryFilters();
+
+//void QueryFilters()
+//{
+//    var auth = _context.Authors.Where(s => s.FirstName == "Josie").ToList();
+//}
+
+
+
+//void AddAuthor()
+//{
+//    var author = new Author { FirstName = "Josie", LastName = "Newf" };
+//    using var context = new PubContext();
+//    context.Authors.Add(author);
+//    context.SaveChanges();
+//}
+//void AddAuthorWithBook()
+//{
+//    var auth = new Author { FirstName = "Julie", LastName = "Lerman" };
+//    auth.Books.Add(new Book
+//    {
+//        Title = "Programming Entity Framework",
+//        PublishDate = new DateTime(2009, 1, 1)
+//    });
+//    auth.Books.Add(new Book
+//    {
+//        Title = "Programming Entity Framework 2nd Ed",
+//        PublishDate = new DateTime(2010, 8, 1)
+//    });
+//    using var context = new PubContext();
+//    context.Authors.Add(auth);
+//    context.SaveChanges();
+//}
+
+//void GetAuthorsWithBooks()
+//{
+//    using var context = new PubContext();
+//    var auths = context.Authors.Include(a=> a.Books).ToList();
+//    foreach (var auth in auths)
+//    {
+//        Console.WriteLine(auth.FirstName + " " + auth.LastName);
+//        foreach (var book in auth.Books)
+//        {
+//            Console.WriteLine("*"+book.Title);
+//        }
+//    }
+//}
 
 void GetAuthors()
 {
-    using var context = new PubContext();
-    var authors = context.Authors.ToList();
+    var authors = _context.Authors.ToList();
     foreach (var auth in authors)
     {
-        Console.WriteLine(auth.FirstName+" "+auth.LastName);
+        Console.WriteLine(auth.FirstName + " " + auth.LastName);
     }
 }
 
--- EFCore6Fundamentals/PublisherData/PubContext.cs
+++ EFCore6Fundamentals/PublisherData/PubContext.cs
@@ -12,7 +12,13 @@ namespace PublisherData
         {
             optionsBuilder.UseSqlServer(
                 "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase"
-            );
+            ).LogTo(Console.WriteLine, new[] {DbLoggerCategory.Database.Command.Name });
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, FirstName = "Rhoda", LastName = "Lerman" });
+
         }
 
     }
